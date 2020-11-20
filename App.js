10cfe992737c8dfdbb2eaaf95dd8b7a568596e70
coolchaeyoung <ggodchae@naver.com>
2020-11-20 22:55:23 +0900

    initial setting

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { AppLoading } from 'expo';
+import * as Font from "expo-font";
+import { Asset } from 'expo-asset';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { persistCache } from 'apollo-cache-persist';
+import AsyncStorage from "@react-native-community/async-storage";
+import ApolloClient from 'apollo-boost';
+import apolloClientOptions from './apollo';
+import { ApolloProvider } from 'react-apollo-hooks';
+import { ThemeProvider } from 'styled-components';
+import styles from './styles';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [loaded, setLoaded] = useState(false);
+  const [client, setClient] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+  const preLoad = async () => {
+    try {
+      await Font.loadAsync(Ionicons.font);
+      await Asset.loadAsync([require("./assets/logo.png")]);
+      const cache = new InMemoryCache();
+      await persistCache({
+        cache,
+        storage: AsyncStorage,
+      });
+      const client = new ApolloClient({
+        cache,
+        ...apolloClientOptions
+      });
+      const isLoggedIn = await AsyncStorage.getItem("isLoggedIn");
+      if (isLoggedIn) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setLoaded(true);
+      setClient(client);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    preLoad();
+  }, []);
+  return loaded && client && isLoggedIn !== null ? (
+    <ThemeProvider theme={styles}>
+        <ApolloProvider client={client}>
+          <View>
+            {isLoggedIn ? <Text>I'm in</Text> : <Text>I'm out</Text>}
+          </View>
+        </ApolloProvider>
+    </ThemeProvider>
+  ) : (
+    <AppLoading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
